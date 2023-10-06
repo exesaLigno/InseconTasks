e34fdd718113bf21a67cc02c95548ddc08e60347
Vadim Karcev <cfg888powerfull@gmail.com>
2023-10-06 14:24:54 +0300

    Added comments for ossl cl's

--- gen_task_1_1.py
+++ gen_task_1_1.py
@@ -21,45 +21,54 @@ if __name__ == "__main__":
     if isdir(workdir): rmtree(workdir)
     mkdir(workdir)
 
+    # Generating RSA-key with aes256 encryption and specified length
     run(["openssl", "genrsa", "-aes256", "-passout", f"pass:{user.name}", "-out", f"{workdir}/{file_prefix}-ca.key", f"{task.ca_keylen}"])
-    run(["openssl", "req", "-x509", "-new", "-key", 
-         f"{workdir}/{file_prefix}-ca.key", "-passin", f"pass:{user.name}", 
-         "-days", f"{task.ca_time}", 
-         "-subj", f"/C=RU/ST=Moscow/L=Moscow/O={user.name}/OU={user.name} P1_1/CN={user.name} CA/emailAddress={user.email}",
-         "-addext", "basicConstraints=critical,CA:TRUE",
-         "-addext", "keyUsage=critical,digitalSignature,keyCertSign,cRLSign",
-         "-out", f"{workdir}/{file_prefix}-ca.crt"])
+    # Generating self-signed certificate with specified RSA key
+    run(["openssl", "req", "-x509", "-new", 
+         "-key", f"{workdir}/{file_prefix}-ca.key", "-passin", f"pass:{user.name}",                                          # Passing encrypted RSA key and password
+         "-days", f"{task.ca_time}",                                                                                         # Setting time limit for certificate
+         "-subj", f"/C=RU/ST=Moscow/L=Moscow/O={user.name}/OU={user.name} P1_1/CN={user.name} CA/emailAddress={user.email}", # Setting certificate parameters in format /param1=value1/param2=value2/...
+         "-addext", "basicConstraints=critical,CA:TRUE",                                                                     # Adding x509v3 extensions
+         "-addext", "keyUsage=critical,digitalSignature,keyCertSign,cRLSign",                                                # Adding x509v3 extensions
+         "-out", f"{workdir}/{file_prefix}-ca.crt"])                                                                         # Setting up output file
 
+    # Generating RSA-key with aes256 encryption and specified length
     run(["openssl", "genrsa", "-aes256", "-passout", f"pass:{user.name}", "-out", f"{workdir}/{file_prefix}-intr.key", f"{task.intr_keylen}"])
+    # Generating certificate signing request
     run(["openssl", "req", "-new", 
-         "-key", f"{workdir}/{file_prefix}-intr.key", "-passin", f"pass:{user.name}", 
-         "-subj", f"/C=RU/ST=Moscow/L=Moscow/O={user.name}/OU={user.name} P1_1/CN={user.name} Intermediate CA/emailAddress={user.email}",
-         "-addext", "basicConstraints=critical,pathlen:0,CA:TRUE",
-         "-addext", "keyUsage=critical,digitalSignature,keyCertSign,cRLSign",
-         "-out", f"{workdir}/{file_prefix}-intr.csr"])
+         "-key", f"{workdir}/{file_prefix}-intr.key", "-passin", f"pass:{user.name}",                                                      # Passing encrypted RSA key and password
+         "-subj", f"/C=RU/ST=Moscow/L=Moscow/O={user.name}/OU={user.name} P1_1/CN={user.name} Intermediate CA/emailAddress={user.email}",  # Setting certificate parameters in format /param1=value1/param2=value2/...
+         "-addext", "basicConstraints=critical,pathlen:0,CA:TRUE",                                                                         # Adding x509v3 extensions
+         "-addext", "keyUsage=critical,digitalSignature,keyCertSign,cRLSign",                                                              # Adding x509v3 extensions 
+         "-out", f"{workdir}/{file_prefix}-intr.csr"])                                                                                     # Setting up output file
+    # Generating certificate from request
     run(["openssl", "x509", "-req", "-days", f"{task.intr_time}",
-         "-CA", f"{workdir}/{file_prefix}-ca.crt", "-CAkey", f"{workdir}/{file_prefix}-ca.key", "-passin", f"pass:{user.name}",
+         "-CA", f"{workdir}/{file_prefix}-ca.crt", "-CAkey", f"{workdir}/{file_prefix}-ca.key", "-passin", f"pass:{user.name}", # Passing ca cerificate with key and password
          #"-CAcreateserial", "-CAserial", f"{workdir}/serial",
-         "-copy_extensions", "copy",
-         "-in", f"{workdir}/{file_prefix}-intr.csr", 
-         "-out", f"{workdir}/{file_prefix}-intr.crt"])
+         "-copy_extensions", "copy",                                                                                            # Copying x509v3 extensions from request to certificate
+         "-in", f"{workdir}/{file_prefix}-intr.csr",                                                                            # Passing request
+         "-out", f"{workdir}/{file_prefix}-intr.crt"])                                                                          # Specifying output path
 
+    # Generating RSA-key without encryption and with specified length
     run(["openssl", "genrsa", "-out", f"{workdir}/{file_prefix}-basic.key", f"{task.basic_keylen}"])
+    # Generating certificate signing request
     run(["openssl", "req", "-new",
-         "-key", f"{workdir}/{file_prefix}-basic.key",
-         "-subj", f"/C=RU/ST=Moscow/L=Moscow/O={user.name}/OU={user.name} P1_1/CN={user.name} Basic/emailAddress={user.email}",
-         "-addext", "basicConstraints=CA:FALSE",
-         "-addext", "keyUsage=critical,digitalSignature",
-         "-addext", "extendedKeyUsage=critical,serverAuth,clientAuth",
-         "-addext", f"subjectAltName=DNS:basic.{user.name}.ru,DNS:basic.{user.name}.com",
-         "-out", f"{workdir}/{file_prefix}-basic.csr"])
+         "-key", f"{workdir}/{file_prefix}-basic.key",                                                                          # Passing RSA key
+         "-subj", f"/C=RU/ST=Moscow/L=Moscow/O={user.name}/OU={user.name} P1_1/CN={user.name} Basic/emailAddress={user.email}", # Setting certificate parameters in format /param1=value1/param2=value2/...
+         "-addext", "basicConstraints=CA:FALSE",                                                                                # Adding x509v3 extensions
+         "-addext", "keyUsage=critical,digitalSignature",                                                                       # Adding x509v3 extensions
+         "-addext", "extendedKeyUsage=critical,serverAuth,clientAuth",                                                          # Adding x509v3 extensions
+         "-addext", f"subjectAltName=DNS:basic.{user.name}.ru,DNS:basic.{user.name}.com",                                       # Adding x509v3 extensions
+         "-out", f"{workdir}/{file_prefix}-basic.csr"])                                                                         # Setting up output file
+    # Generating certificate from request
     run(["openssl", "x509", "-req", "-days", f"{task.basic_time}",
-         "-CA", f"{workdir}/{file_prefix}-intr.crt", "-CAkey", f"{workdir}/{file_prefix}-intr.key", "-passin", f"pass:{user.name}",
+         "-CA", f"{workdir}/{file_prefix}-intr.crt", "-CAkey", f"{workdir}/{file_prefix}-intr.key", "-passin", f"pass:{user.name}", # Passing intr cerificate with key and password
          #"-CAcreateserial", "-CAserial", f"{workdir}/serial",
-         "-copy_extensions", "copy",
-         "-in", f"{workdir}/{file_prefix}-basic.csr",
-         "-out", f"{workdir}/{file_prefix}-basic.crt"])
-    
+         "-copy_extensions", "copy",                                                                                                # Copying x509v3 extensions from request to certificate
+         "-in", f"{workdir}/{file_prefix}-basic.csr",                                                                               # Passing request
+         "-out", f"{workdir}/{file_prefix}-basic.crt"])                                                                             # Specifying output path
+     
+    # Generating archive with solution
     with ZipFile(archive_name, "w") as archive:
         for directory, _, files in walk(workdir):
             for file in files:
